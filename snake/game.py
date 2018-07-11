49c81d28f687399fe276ff4f6d6af73771d71578
moonblade <moonblade168@gmail.com>
2018-07-12 00:37:50 +0530

    its learning, finally

--- snake/game.py
+++ snake/game.py
@@ -10,25 +10,30 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
-
-maxStep = 10000
-perNet = 5
+render = False
+maxStep = 200
+perNet = 1
+checkPoint = 49
 def eval_genome(genome, config):
 	net = neat.nn.FeedForwardNetwork.create(genome, config)
 	fitnesses = []
 	for x in range(perNet):
 		observation = env.reset()
 		step = 0
-		totReward = 0
+		# totReward = 0
 		while True and step<maxStep:
-			env.render()
 			step+=1
 			action = np.argmax(net.activate(observation))
 			observation, reward, done = env.step(action)
-			totReward += reward
+			# totReward += reward
 			if done:
 				break
-		fitnesses.append(totReward+step*0.001)
+		totReward = len(env.snake.snake)
+		if (totReward<10):
+			fitnesses.append(step*step*(2**totReward))
+		else:
+			fitnesses.append(step*step*(2**10)*totReward-9)
+
 	return min(fitnesses)
 
 def eval_genomes(genomes, config):
@@ -37,8 +42,10 @@ def eval_genomes(genomes, config):
 
 def run(path):
 	config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, path)
-	# p = neat.Population(config)
-	p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-124')
+	if checkPoint:
+		p = neat.Checkpointer.restore_checkpoint('neat-checkpoint-'+str(checkPoint))
+	else:
+		p = neat.Population(config)
 
 	p.add_reporter(neat.StdOutReporter(True))
 	stat = neat.StatisticsReporter()
@@ -46,8 +53,10 @@ def run(path):
 	p.add_reporter(neat.Checkpointer(50))
 
 	pe = neat.ParallelEvaluator(20, eval_genome)
-	# winner = p.run(pe.evaluate)
-	winner = p.run(eval_genomes, 300)
+	if render:
+		winner = p.run(eval_genomes, 300)
+	else:
+		winner = p.run(pe.evaluate)
 
 	print('\nBest genome:\n{!s}'.format(winner))
 	winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
@@ -63,9 +72,9 @@ class Snake():
 	# 0 right, 1 left, 2 up, 3 down
 	def __init__(self, board):
 		self.board = board
-		self.startLength = 2
+		self.startLength = 4
 		self.reset()
-		self.lengthIncrease = 1
+		self.lengthIncrease = 4
 		self.length = 0
 
 	def reset(self):
@@ -105,6 +114,32 @@ class Snake():
 			return None
 		return nextTile
 
+	def look(self, direction):
+		now = np.array(self.snake[-1, :]) #head
+		tail=False
+		food=False
+		dist = 1
+		retval = [0, 0, 0]
+		while (now[0]>=0 and now[0]<self.board.size and now[1]>=0 and now[1]<self.board.size):
+			now+=direction
+			if not food and np.array_equal(self.board.dot.dot, now):
+				retval[0]=1/dist
+				food=True
+			if not tail and (self.snake == now).all():
+				retval[1]=1/dist
+				tail = True
+			dist+=1
+		retval[2]=1/dist
+		return retval
+
+	def getState(self):
+		state=[]
+		for x in range(-1,2):
+			for y in range(-1,2):
+				if x!=0 or y!=0:
+					state+=self.look(np.array([x,y]))
+		return state
+
 	def move(self):
 		tile = self.getNextTile()
 		if self.dead:
@@ -135,7 +170,7 @@ class Board():
 	# 1 is snakeHead
 	# 2 is snake
 	# 3 is food
-	def __init__(self, size=5):
+	def __init__(self, size=10):
 		self.width = 10
 		self.margin = 1
 		self.size = size
@@ -147,6 +182,9 @@ class Board():
 		return np.argwhere(self.board==0) if ignoreSnake else np.array([x for x in np.argwhere(self.board==0).tolist() if x not in self.snake.snake.tolist()])
 
 	def getState(self):
+		return self.snake.getState()
+
+	def getStateOld(self):
 		state = self.board
 		snake = np.transpose(self.snake.snake)
 		state[snake[0], snake[1]]=2
@@ -159,10 +197,11 @@ class Board():
 		ate, dead = self.snake.step(direction)
 		if ate:
 			self.dot.reset()
-			reward = 1
+			reward = len(self.snake.snake)
 		else:
 			reward = 0
-		# self.render()
+		if render:
+			self.render()
 		return self.getState(), reward, dead
 
 	def render(self):
